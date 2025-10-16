6ea60a01340af9f051703993214136698331ea4a
matheusmansur694 <matheus.toniazzo01093752@sesisenaipr.org.br>
2025-10-16 08:49:58 -0300

    Add files via upload

--- backend/databasePostgres.js
+++ backend/databasePostgres.js
@@ -1,43 +1,53 @@
-import { randomUUID } from "node:crypto"
-import { sql } from "./sql.js";
-import bcrypt from "bcrypt"
+import { randomUUID } from "node:crypto";
+import { sql } from './sql.js';
+import bcrypt from 'bcrypt';
 
-export class DatabasePostgres{
-    async lis(){
+
+export class DatabasePostgres {
+    async list() {
         try{
-            const result = await sql `SELECT * FROM USERS`;
+            const result = await sql`SELECT * FROM users;`;
             return result;
-        }catch (err){
-            console.log("Erro ao listar usuário: ", err);
-            return[];
+        } catch (err){
+            console.log("Erro ao listar usuários: ", err);
+            return [];
         }
     }
 
-    async create(user){
-        const userId = randomUUID();
-        const { name, email, password } = user;
-        const hashedPassword = await bcrypt.hash(password, 10);
 
+    async create(user) {
+    const userId = randomUUID();
+    const { name, email, password } = user;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+
+       
         await sql`
-            INSERT INTO users (id, name, email, password) VALUES (${userId}, ${name},
-            ${email}, ${hashedPassword})
+        INSERT INTO users (id, name, email, password)
+        VALUES (${userId}, ${name}, ${email}, ${hashedPassword})
         `;
     }
 
-    async findByEmail(email){
+
+    async findByEmail(email) {
         const result = await sql`SELECT * FROM users WHERE email = ${email};`;
         return result[0];
     }
+
 
     async update(id, user){
         const { name, email } = user;
         await sql`
             UPDATE users
             SET name = ${name}, email = ${email}
-            WHERE id = ${id}        
-        `
+            WHERE id = ${id}
+        `;
     }
+
+
     async delete(id){
         await sql`DELETE FROM users WHERE id = ${id}`;
     }
 }
+
+
--- backend/sql.js
+++ backend/sql.js
@@ -1,7 +1,11 @@
-import postgres from "postgres";
-import dotenv from "dotenv"
+import postgres from "postgres";                                                          
+import dotenv from "dotenv"                                                               
 dotenv.config();
 
 export const sql = postgres(process.env.DATABASE_URL, {
     ssl: 'require'
 });
+
+//DATABASE_URL='postgresql://neondb_owner:npg_m2l7ayiFhADP@ep-cool-mouse-acszggav-pooler.
+//sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+//JWT_SECRET=suaChaveUltraSecreta
