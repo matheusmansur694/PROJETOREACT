bcb282cf077ff0db91b23117144383d7e4c60b49
matheusmansur694 <matheus.toniazzo01093752@sesisenaipr.org.br>
2025-10-16 12:17:11 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -85,6 +85,32 @@ app.get('/protected', (req, res) => {
 });
 
 app.get('/users', async(req, res) => {
-  const users = await.database.list();
+  const users = await database.list();
   res.json(users);
 })
+
+app.post('/users', async(req, res) => {
+  const { name, email, password } = req.body;
+  if(!name || !email || !password){
+    return res.status(400).json({ msg: 'Preencha todos os campos!'})
+  }
+  await database.create({ name, email, password})
+  res.status(201).send()
+});
+
+app.put('/users/:id', async(req, res) => {
+  const id = req.params.id;
+  const user = req.body;
+  await database.update(id, user);
+  res.status(204).send()
+})
+
+app.delete('/users/:id', async(req, res) => {
+  const id = req.params.id;
+  await database.delete(id);
+  res.status(204).send();
+});
+
+app.listen(3001, () => {
+  console.log('Servidor rodando na porta 3001');
+});
